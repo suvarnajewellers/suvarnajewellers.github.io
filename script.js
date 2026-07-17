3e00a636f1095ef9ec9e9c80c6ca2ea885b5b87b
suvarnajewellers <jewellerssuvarna@gmail.com>
2026-07-17 23:21:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
 
+    const images = document.querySelectorAll(".gallery img");
+
+    images.forEach(img => {
+        img.addEventListener("click", function () {
+            alert("Full Screen Gallery આગળના સ્ટેપમાં ઉમેરશું.");
+        });
+    });
+
+});
