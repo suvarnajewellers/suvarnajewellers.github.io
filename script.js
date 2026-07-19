7f78536ef4d4e944b155348b6ba49fc1b132c715
suvarnajewellers <jewellerssuvarna@gmail.com>
2026-07-19 21:03:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -285,3 +285,15 @@ scrollBtn.onclick = function () {
         behavior: "smooth"
     });
 };
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+if(menuToggle && navLinks){
+
+    menuToggle.addEventListener("click", function(){
+
+        navLinks.classList.toggle("active");
+
+    });
+
+}
