bcebeef5de5138c90583a6589195b589af5a2223
suvarnajewellers <jewellerssuvarna@gmail.com>
2026-07-21 22:41:46 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,20 @@
 
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyAYBr5skg63NbaqOQgDj_WsteCzLTZp2IM",
+    authDomain: "suvarna-jewellers-17e4e.firebaseapp.com",
+    projectId: "suvarna-jewellers-17e4e",
+    storageBucket: "suvarna-jewellers-17e4e.firebasestorage.app",
+    messagingSenderId: "850094802552",
+    appId: "1:850094802552:web:e0eccebc206437f7660f8d"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
