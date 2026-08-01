4aca82ef3d9827d060f1f0109d4939ae742836b4
suvarnajewellers <jewellerssuvarna@gmail.com>
2026-08-01 10:04:56 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -266,36 +266,3 @@ function initScrollTop(){
 
 
   }
-
-/* ==========================
-   HIDE LOADER
-========================== */
-
-function hideLoader(){
-
-    const loader = document.getElementById("loader");
-
-    if(loader){
-
-        loader.style.display = "none";
-
-    }
-
-}
-
-
-/* ==========================
-   SCROLL TO TOP
-========================== */
-
-function scrollToTop(){
-
-    window.scrollTo({
-
-        top: 0,
-
-        behavior: "smooth"
-
-    });
-
-}
